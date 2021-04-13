cb7805941b75075e1ee4ecf8ba2adc65d024df5d
estefaniatc <estefania.torres1@upr.edu>
2021-04-13 17:35:11 -0400

    environ variable

--- util/config.py
+++ util/config.py
@@ -1,13 +1,18 @@
 import flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 DEV_DB = 'postgresql://aslesusr:1234@localhost/SignLanguagePR'
-PROD_DB = 'postgres://oznwwrrmbzaouj:c3734637920c26912d7cebad22d33eb971c8925425c4f2bf342d326bd1d4cb1a@ec2-3-211-37-117.compute-1.amazonaws.com:5432/d2cm180srol83l'
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DEV_DB
 app.secret_key = "asles"
+
+if 'DATABASE_URL' in environ:
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = DEV_DB
+
 
 db = SQLAlchemy(app)
 CORS(app)
