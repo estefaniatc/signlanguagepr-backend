04fa7729f0d903e5344e96f24f913ad14ab42853
estefaniatc <estefania.torres1@upr.edu>
2021-04-24 14:01:15 -0400

    added model name

--- dao/models.py
+++ dao/models.py
@@ -8,6 +8,7 @@ class Models(db.Model):
     question = db.Column(db.String(500), nullable=False)
     answer = db.Column(db.String(100), nullable=False)
     xp = db.Column(db.Integer, nullable=True)
+    model_name = db.Column(db.String(50), nullable=True)
 
     def __init__(self, **args):
         self.lesson_id = args.get('lesson_id')
@@ -15,6 +16,7 @@ class Models(db.Model):
         self.question = args.get('question')
         self.answer = args.get('answer')
         self.xp = args.get('xp')
+        self.model_name = args.get('model_name')
 
     @property
     def pk(self):
